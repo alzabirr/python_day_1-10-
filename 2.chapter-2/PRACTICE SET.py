118a3c8f920cc27b9ed9cd09639d952d1bd5c719
alzabirr <alzabirhussain@gmail.com>
2025-06-29 00:20:46 +0600

    day1

---
+++ 2.chapter-2/PRACTICE SET.py
@@ -0,0 +1,54 @@
+"""
+1. Write a python program to add two numbers.
+2. Write a python program to find remainder when a number is divided by z.
+3. Check the type of variable assigned using input () function.
+4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+‘b’ or not. Take a = 34 and b = 80
+5. Write a python program to find an average of two numbers entered by the user.
+6. Write a python program to calculate the square of a number entered by the user.
+
+
+"""
+
+# 1. Write a python program to add two numbers.
+# print(1+4)
+
+
+
+# 2. Write a python program to find remainder when a number is divided by z.
+# number1 = int(input("enter u number 1 :"))
+# number2 = int(input("enter u number 2 :"))
+# remainder = number1 % number2
+# print( "remainder is :", remainder)
+
+
+# 3. Check the type of variable assigned using input () function
+# value = input("enter your name")
+# print(type( value))
+
+
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+
+# a = 34
+# b= 80
+
+# if a > b: 
+#     print("a is big")
+# else:
+#     print("a is not big ")
+
+
+# 5. Write a python program to find an average of two numbers entered by the user.
+
+# number_1 = int(input("enter u number 1 :"))
+# number_2 = int(input("enter u number 2 :" ))
+
+# average =( number_1 + number_2) / 2 
+# print(average)
+
+
+# 6. Write a python program to calculate the square of a number entered by the user.
+
+# square_of_a_numbe = int(input("enter your number : "))
+# square  = square_of_a_numbe ** 2
+# print(square)
